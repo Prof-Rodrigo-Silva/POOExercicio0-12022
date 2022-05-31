ac92ff76734a8e2260c64f326ff7688cfbd7c74e
Rodrigo Silva <33011697+Prof-Rodrigo-Silva@users.noreply.github.com>
2022-05-31 09:23:29 -0300

    Add files via upload

---
+++ Ex0/Ex0.java
@@ -0,0 +1,28 @@
+
+ class Pessoa{
+	 public static void main(String[] args) {
+	    
+	    String nome="Ciclano";
+	    String nomeMae="Fulana";
+	    int idade=33;
+	    long celular= 333333333;
+	    System.out.println("Nome: "+ nome);
+	    System.out.println("Idade: "+idade);
+	    System.out.println("Celular : " +celular);
+	    System.out.println("Nome da Mãe: " +nomeMae);
+	
+	}
+}
+class Conta{
+    public static void main(String[] args) {
+    int id=123;
+    String correntista="Beltrano";  
+    float saldo=2500.67f;
+    System.out.println("Id: "+ id);
+    System.out.println("Correntista: " +correntista);
+    System.out.println("Saldo : " +saldo);
+	
+	
+    	    
+    	}
+}
